d4969f59be86767250ec4d356168fa927f4b573a
Ezra Lazuardy <ezralucio@gmail.com>
2018-10-21 17:32:56 +0700

    Update

--- application/controllers/C_Tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-class C_Tables extends CI_Controller {
-	public function index() {
-		$this->load->view("V_Header");
-		$this->load->view("V_Sidebar");
-		$this->load->view("MainMenu/V_Tables");
-		$this->load->view("V_Footer");
-	}
-}
